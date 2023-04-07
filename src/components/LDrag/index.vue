601ae721ca9be9dd677c11e56feaf95ba381c170
DragonLJH <1183665002@qq.com>
2023-04-07 18:29:46 +0800

    更新拖拉交换组件基础版

--- src/components/LDrag/index.vue
+++ src/components/LDrag/index.vue
@@ -1,51 +1,75 @@
 <template>
-    <div class="l-drag" @drop="drop" @dragover="dragover" @dragstart="dragstart">
-        <div class="item" v-for="(item, index) in list" :key="item.id" draggable="true" :data-id="index">
+    <div class="l-drag" @drop="drop" @dragover="dragover" @dragstart="dragstart" @dragenter="dragenter">
+        <div class="item" :class="activeId == index ? 'active' : ''" v-for="(item, index) in data.list" :key="item.id"
+            draggable="true" :data-id="item.id" :data-index="index">
             {{ item.title }}
         </div>
     </div>
 </template>
 <script setup lang="ts">
-import { withDefaults, defineEmits, onMounted, reactive } from "vue"
+import { withDefaults, defineEmits, onMounted, reactive, ref } from "vue"
 type LDragProps = {
     id: number
     title: string
 }
-const name = "LDrag"
 const props = withDefaults(defineProps<{
     list: Array<LDragProps>
 }>(), {
     list: () => {
         return [
-            { id: 0, title: "title-0" },
-            { id: 1, title: "title-1" },
-            { id: 2, title: "title-2" },
-            { id: 3, title: "title-3" },
+            { id: 0, title: "" }
         ]
     }
 })
 const emit = defineEmits(["update:list"])
-const list = reactive(props.list)
+
+const activeId = ref(-1)
+const startIndex = ref(-1)
+const data = reactive({ list: props.list })
+
 const drop = (e: any) => {
-    const startId = e.dataTransfer.getData("startId");
-    const { id } = e.target.dataset
-    const o = [...list].splice(startId, 1)
-    const n = [...list].splice(id, 1)
-    list.splice(id, 1, ...o)
-    list.splice(startId, 1, ...n)
-    emit("update:list", list)
+    activeId.value = -1
+    const startIndex = e.dataTransfer.getData("startIndex");
+    const { index } = e.target.dataset
+    const newList = [...props.list]
+    // const o = [...props.list].splice(startIndex, 1)
+    // const n = [...props.list].splice(index, 1)
+    // newList.splice(index, 1, ...o)
+    // newList.splice(startIndex, 1, ...n)
+    const o = [...props.list].splice(startIndex, 1)[0]
+    newList.splice(startIndex, 1)
+    newList.splice(index, 0, o)
+    console.log("newList", newList)
+    // emit("update:list", newList)
+}
+const dragstart = (e: any) => {
+    const { index, id } = e.target.dataset
+    activeId.value = index
+    e.dataTransfer.setData("startIndex", index);
+    e.dataTransfer.setData("startId", id);
+    console.log("dragstart", e.dataTransfer.getData("startIndex"))
 }
 const dragover = (e: any) => {
     e.preventDefault();
     e.stopPropagation();
 }
-const dragstart = (e: any) => {
-    const { id } = e.target.dataset
-    e.dataTransfer.setData("startId", id);
-    console.log("dragstart", id)
+const dragenter = (e: any) => {
+    const startIndex = e.dataTransfer.getData("startIndex");
+    const { index } = e.target.dataset
+    const newList = [...data.list]
+    let o = newList[index]
+    newList.splice(startIndex, 1, o)
+    console.log(newList)
+    data.list = newList
+    e.preventDefault();
+    e.stopPropagation();
 }
 onMounted(() => {
-    console.log(list)
+    data.list = props.list
 })
 </script>
-<style></style>
+<style>
+.item.active {
+    background-color: #ccc;
+}
+</style>
--- src/view/DragView/index.vue
+++ src/view/DragView/index.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="drag-view">
-    <LDrag v-model:list="list" />
+    <LDrag v-model:list="data.list" />
   </div>
 </template>
 <script lang="ts" setup>
 import LDrag from '@/components/LDrag/index.vue'
 import { reactive } from 'vue';
-const list = reactive([
-  { id: 0, title: "title-0" },
-  { id: 1, title: "title-1" },
-  { id: 2, title: "title-2" },
-  { id: 3, title: "title-3" },
-])
+let data = reactive({
+  list: [
+    { id: 0, title: "title-0" },
+    { id: 1, title: "title-1" },
+    { id: 2, title: "title-2" },
+    { id: 3, title: "title-3" },
+  ]
+})
+
 </script>

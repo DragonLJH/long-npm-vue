c6c455a8d9fffff09334d3a598c428c1b848ed4f
DragonLJH <1183665002@qq.com>
2023-04-10 18:28:25 +0800

    更新拖拉交换组件基础版

--- src/components/LDrag/index.vue
+++ src/components/LDrag/index.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="l-drag" @drop="drop" @dragover="dragover" @dragstart="dragstart" @dragenter="dragenter">
-        <div class="item" :class="activeId == index ? 'active' : ''" v-for="(item, index) in data.list" :key="item.id"
+        <div class="item" :class="activeIndex == index ? 'active' : ''" v-for="(item, index) in data.list" :key="item.id"
             draggable="true" :data-id="item.id" :data-index="index">
             {{ item.title }}
         </div>
@@ -23,28 +23,28 @@ const props = withDefaults(defineProps<{
 })
 const emit = defineEmits(["update:list"])
 
-const activeId = ref(-1)
-const startIndex = ref(-1)
+const activeIndex = ref(-1)
+const dragenterIndex = ref(-1)
 const data = reactive({ list: props.list })
 
 const drop = (e: any) => {
-    activeId.value = -1
-    const startIndex = e.dataTransfer.getData("startIndex");
-    const { index } = e.target.dataset
-    const newList = [...props.list]
-    // const o = [...props.list].splice(startIndex, 1)
-    // const n = [...props.list].splice(index, 1)
-    // newList.splice(index, 1, ...o)
-    // newList.splice(startIndex, 1, ...n)
-    const o = [...props.list].splice(startIndex, 1)[0]
-    newList.splice(startIndex, 1)
-    newList.splice(index, 0, o)
-    console.log("newList", newList)
-    // emit("update:list", newList)
+    activeIndex.value = -1
+    dragenterIndex.value = - 1
+    // const startIndex = e.dataTransfer.getData("startIndex");
+    // const { index } = e.target.dataset
+    // const newList = [...props.list]
+    // // const o = [...props.list].splice(startIndex, 1)
+    // // const n = [...props.list].splice(index, 1)
+    // // newList.splice(index, 1, ...o)
+    // // newList.splice(startIndex, 1, ...n)
+    // const o = [...props.list].splice(startIndex, 1)[0]
+    // newList.splice(startIndex, 1)
+    // newList.splice(index, 0, o) 
+    emit("update:list", data.list)
 }
 const dragstart = (e: any) => {
     const { index, id } = e.target.dataset
-    activeId.value = index
+    activeIndex.value = index
     e.dataTransfer.setData("startIndex", index);
     e.dataTransfer.setData("startId", id);
     console.log("dragstart", e.dataTransfer.getData("startIndex"))
@@ -54,13 +54,18 @@ const dragover = (e: any) => {
     e.stopPropagation();
 }
 const dragenter = (e: any) => {
-    const startIndex = e.dataTransfer.getData("startIndex");
-    const { index } = e.target.dataset
-    const newList = [...data.list]
-    let o = newList[index]
-    newList.splice(startIndex, 1, o)
-    console.log(newList)
-    data.list = newList
+    const { index, id } = e.target.dataset
+    // 解决频繁拖拽交换
+    // 
+    if (id !== dragenterIndex.value) { 
+        const newList = [...data.list]
+        const o = [...data.list].splice(activeIndex.value, 1)[0]
+        newList.splice(activeIndex.value, 1)
+        newList.splice(index, 0, o)
+        data.list = newList
+        activeIndex.value = index
+        return
+    }
     e.preventDefault();
     e.stopPropagation();
 }
